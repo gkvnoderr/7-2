395f20de328f5ba185b0414c41d57e9b0e08c6b0
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-10-19 09:35:16 +0300

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Задача 1
+            //Console.Write("Введите название футбольного клуба: ");
+            //string name = Convert.ToString(Console.ReadLine());
+            //int amount = 0;
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    amount++;
+            //}
+            //Console.WriteLine($"Количество символов в названии: {amount}");
+
+            //Задача 2
+            //Console.Write("Введите название города: ");
+            //string name = Convert.ToString(Console.ReadLine());
+            //int amount = 0;
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    amount++;
+            //}
+            //if (amount % 2 == 0)
+            //{ Console.WriteLine("Количество символов в названии чётное"); }
+            //else { Console.WriteLine("Количество символов в названии нечётное"); }
+
+            //Задача 3
+            //Console.Write("Введите фамилию: ");
+            //string name1 = Convert.ToString(Console.ReadLine());
+            //Console.Write("Введите вторую фамилию: ");
+            //string name2 = Convert.ToString(Console.ReadLine());
+            //if (name1.Length > name2.Length)
+            //{ Console.WriteLine("Первая фамилия длиннее второй"); }
+            //else if (name1.Length < name2.Length)
+            //{ Console.WriteLine("Вторая фамилия длиннее первой"); }
+            //else { Console.WriteLine("Фамилии одинаковые по длине"); }
+
+            //Задача 4
+           
+            //Задача 5
+            //Console.WriteLine("Введите имя:");
+            //string first_name = Convert.ToString(Console.ReadLine());
+            //Console.WriteLine("Введите фамилию:");
+            //string last_name = Convert.ToString(Console.ReadLine());
+            //Console.WriteLine($"{first_name}{last_name}");
+
+            //Задача 6
+            //Console.Write("Введите слово: ");
+            //string word = Convert.ToString(Console.ReadLine());
+            //int count = 0;
+            //foreach (char a in word)
+            //{
+            //    if (a == 'а')
+            //    { count++; }
+            //    if (a == 'А')
+            //    { count++; }
+            //}
+            //Console.WriteLine(count);
+
+            //Задача 7
+            //Console.Write("Введите строку: ");
+            //string str = Console.ReadLine();
+            //string[] words = str.Split(" ".ToCharArray());
+            //for (int i = 0; i < words.Length; i++)
+            //{ }
+            //Console.WriteLine($"Количество слов в строке:{words.Length} ");
+            //Console.ReadKey();
+        }
+    }
+}
